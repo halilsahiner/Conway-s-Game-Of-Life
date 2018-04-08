863a247e671574cc13df0759509c5280eae0f676
halilsahiner <halil.sahiner@ug.bilkent.edu.tr>
2018-04-09 00:45:16 +0300

    Initial commit for Conway's game of life simulation

---
+++ src/index.js
@@ -0,0 +1,257 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Box extends React.Component {
+    selectBox = () => {
+        this.props.selectBox(this.props.row, this.props.col);
+    }
+
+    render() {
+        return (
+            <div
+                className={this.props.boxClass}
+                id={this.props.id}
+                onClick={this.selectBox}
+            />
+        );
+    }
+}
+
+class Grid extends React.Component {
+    render() {
+        const width = (this.props.cols * 14);
+        var rowsArr = [];
+
+        var boxClass = "";
+        for (var i = 0; i < this.props.rows; i++) {
+            for (var j = 0; j < this.props.cols; j++) {
+                let boxId = i + "_" + j;
+
+                boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+                rowsArr.push(
+                    <Box
+                        boxClass={boxClass}
+                        key={boxId}
+                        boxId={boxId}
+                        row={i}
+                        col={j}
+                        selectBox={this.props.selectBox}
+                    />
+                );
+            }
+        }
+
+        return (
+            <div className="grid" style={{width: width}}>
+                {rowsArr}
+            </div>
+        );
+    }
+}
+
+class Buttons extends React.Component {
+
+    handleSelect = (evt) => {
+        this.props.gridSize(evt);
+    }
+
+    render() {
+        return (
+            <div className="center">
+                <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                    <div className="btn-group mr-2" role="group" aria-label="First group">
+                        <button type="button" className="btn btn-secondary" onClick={this.props.playButton}>
+                            Play
+                        </button>
+                        <button type="button" className="btn btn-secondary" onClick={this.props.pauseButton}>
+                            Pause
+                        </button>
+                        <button type="button" className="btn btn-secondary" onClick={this.props.clear}>
+                            Clear
+                        </button>
+                        <button type="button" className="btn btn-secondary" onClick={this.props.seed}>
+                            Seed
+                        </button>
+                    </div>
+                    <div className="btn-group mr-2" role="group" aria-label="Second group">
+                        <button type="button" className="btn btn-secondary" onClick={this.props.fast}>
+                            &lsaquo;&lsaquo;
+                        </button>
+                        <button type="button" className="btn btn-secondary" onClick={this.props.slow}>
+                            &rsaquo;&rsaquo;
+                        </button>
+                    </div>
+                    <div className="btn-group mr-2" role="group" aria-label="Third group">
+                        <div className="dropdown">
+                            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"  >
+                                Grid Size
+                            </button>
+                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <button className="dropdown-item" onClick={()=>this.handleSelect("1")}>20x10</button>
+                                <button className="dropdown-item" onClick={()=>this.handleSelect("2")}>50x30</button>
+                                <button className="dropdown-item" onClick={()=>this.handleSelect("3")}>70x50</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+class Main extends React.Component {
+    constructor() {
+        super();
+        this.speed = 128;
+        this.rows = 30;
+        this.cols = 50;
+
+        this.state = {
+            generation: 0,
+            gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+        }
+    }
+
+    selectBox = (row, col) => {
+        let gridCopy = arrayClone(this.state.gridFull);
+        gridCopy[row][col] = !gridCopy[row][col];
+        this.setState({
+            gridFull: gridCopy
+        });
+    };
+
+    seed = () => {
+        let gridCopy = arrayClone(this.state.gridFull);
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                if (Math.floor(Math.random() * 4) === 1) {
+                    gridCopy[i][j] = true;
+                }
+            }
+        }
+        this.setState({
+            gridFull: gridCopy
+        });
+    };
+
+    playButton = () => {
+        clearInterval(this.intervalId);
+        this.intervalId = setInterval(this.play, this.speed);
+    };
+
+    pauseButton = () => {
+        clearInterval(this.intervalId);
+    };
+
+    slow = () => {
+        this.speed = this.speed * 2;
+        this.playButton();
+    };
+
+    fast = () => {
+        this.speed = this.speed / 2;
+        this.playButton();
+    };
+
+    clear = () => {
+        this.pauseButton();
+        var grid = Array(this.rows).fill().map(() => Array(this.cols).fill(false));
+        this.setState({
+            gridFull: grid,
+            generation: 0
+        });
+    };
+
+    gridSize = (size) => {
+        switch (size) {
+            case "1":
+                this.cols = 20;
+                this.rows = 10;
+                break;
+            case "2":
+                this.cols = 50;
+                this.rows = 30;
+                break;
+            default:
+                this.cols = 70;
+                this.rows = 50;
+        }
+        this.clear();
+
+    };
+
+    play = () => {
+        let g = this.state.gridFull;
+        let g2 = arrayClone(this.state.gridFull);
+
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                let count = 0;
+                if (i > 0) if (g[i - 1][j]) count++;
+                if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+                if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+                if (j < this.cols - 1) if (g[i][j + 1]) count++;
+                if (j > 0) if (g[i][j - 1]) count++;
+                if (i < this.rows - 1) if (g[i + 1][j]) count++;
+                if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+                if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+                if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+                if (!g[i][j] && count === 3) g2[i][j] = true;
+            }
+        }
+        this.setState({
+            gridFull: g2,
+            generation: this.state.generation + 1
+        });
+
+    };
+
+    componentDidMount() {
+        this.seed();
+        this.playButton();
+    }
+
+    render() {
+        return <div>
+            <h1>The Game of Life</h1>
+            <Buttons
+                playButton={this.playButton}
+                pauseButton={this.pauseButton}
+                slow={this.slow}
+                fast={this.fast}
+                clear={this.clear}
+                seed={this.seed}
+                gridSize={this.gridSize}
+            />
+            <Grid
+                gridFull={this.state.gridFull}
+                rows={this.rows}
+                cols={this.cols}
+                selectBox={this.selectBox}
+            />
+            <h2>Generations: {this.state.generation}</h2>
+            <h3>Current delay: {this.speed} ms</h3>
+            <div className="info">
+                <p>This is a simulation of Conway's Game Of Life which has the following rules:</p>
+                <ol>
+                    <li>Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.</li>
+                    <li>Any live cell with two or three live neighbours lives on to the next generation.</li>
+                    <li>Any live cell with more than three live neighbours dies, as if by overpopulation.</li>
+                    <li>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</li>
+                </ol>
+            </div>
+
+
+
+        </div>;
+    }
+}
+
+function arrayClone(arr) {
+    return JSON.parse(JSON.stringify(arr));
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
